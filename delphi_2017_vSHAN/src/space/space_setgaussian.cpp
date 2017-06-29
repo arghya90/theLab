5d68115bc99cab8cd04adae9203a599a9c9825ae
Arghya Chakravorty <arghyac@g.clemson.edu>
2017-06-29 00:36:15 -0400

    Changed SRFDEN that corres. to SRFCUT=20 in vac

--- delphi_2017_vSHAN/src/space/space_setgaussian.cpp
+++ delphi_2017_vSHAN/src/space/space_setgaussian.cpp
@@ -672,6 +672,7 @@ void CDelphiSpace::setGaussian()
 		// epstemp=repsin*(1-srfcut)+repsout*srfcut
         epstemp=srfcut;
 		delphi_real shan_sigma = 8.0;
+        delphi_real srfden = 0.759493670886076;
         
 		if(epstemp<repsin)
         {
@@ -686,21 +687,21 @@ void CDelphiSpace::setGaussian()
             {
                 for(iz=1; iz<=iGrid; iz++)
                 {
-                    if(gepsmp2[ix][iy][iz].nX < 0.5)
+                    if(gepsmp2[ix][iy][iz].nX < srfden)
                     {
                         //gepsmp2[ix][iy][iz].nX=repsin;
                         //gepsmp2[ix][iy][iz].nX=1;
                         //gepsmp2[ix][iy][iz].nX=1 + (epstemp - 1)*exp(epstemp - gepsmp2[ix][iy][iz].nX);
                         gepsmp2[ix][iy][iz].nX = 1 + (gepsmp2[ix][iy][iz].nX - 1)*exp(-(gepsmp2[ix][iy][iz].nX - epstemp)*(gepsmp2[ix][iy][iz].nX - epstemp)/(shan_sigma*shan_sigma));
                     }// if
-                    if(gepsmp2[ix][iy][iz].nY < 0.5)
+                    if(gepsmp2[ix][iy][iz].nY < srfden)
                     {
                         //gepsmp2[ix][iy][iz].nY=repsin;
                         //gepsmp2[ix][iy][iz].nY=1;
                         //gepsmp2[ix][iy][iz].nY=1 + (epstemp - 1)*exp(epstemp - gepsmp2[ix][iy][iz].nY);
                         gepsmp2[ix][iy][iz].nY = 1 + (gepsmp2[ix][iy][iz].nY - 1)*exp(-(gepsmp2[ix][iy][iz].nY - epstemp)*(gepsmp2[ix][iy][iz].nY - epstemp)/(shan_sigma*shan_sigma));
                     }// if
-                    if(gepsmp2[ix][iy][iz].nZ < 0.5)
+                    if(gepsmp2[ix][iy][iz].nZ < srfden)
                     {
                         //gepsmp2[ix][iy][iz].nZ=repsin;
                         //gepsmp2[ix][iy][iz].nZ=1;
